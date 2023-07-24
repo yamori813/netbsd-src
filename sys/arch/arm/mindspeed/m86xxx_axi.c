b1dea2e4b4d4f4702178c4e6a2db2d8a53e142e0
Hiroki Mori <yamori813@yahoo.co.jp>
2023-07-24 11:46:24 +0900

    same critical way m83xxx_ahb

--- sys/arch/arm/mindspeed/m86xxx_axi.c
+++ sys/arch/arm/mindspeed/m86xxx_axi.c
@@ -51,8 +51,8 @@ struct axi_softc {
 static int axi_match(device_t, struct cfdata *, void *);
 static void axi_attach(device_t, device_t, void *);
 static int axi_search(device_t, struct cfdata *, const int *, void *);
-static int axi_critical_search(device_t, struct cfdata *, const int *, void *);
 static int axi_search(device_t, struct cfdata *, const int *, void *);
+static void axi_attach_critical(struct axi_softc *);
 static int axi_print(void *, const char *);
 
 CFATTACH_DECL_NEW(axi, sizeof(struct axi_softc),
@@ -77,45 +77,87 @@ axi_attach(device_t parent __unused, device_t self, void *aux __unused)
 	aprint_naive("\n");
 
 	sc = device_private(self);
+	sc->sc_dev = self;
 	sc->sc_iot = &m83_bs_tag;
 	sc->sc_dmat = &m83_bus_dma_tag;
+
+	axi_attach_critical(sc);
 
 	aa.aa_name = "axi";
 	aa.aa_iot = sc->sc_iot;
 	aa.aa_dmat = sc->sc_dmat;
 	config_search(self, &aa,
-	    CFARGS(.search = axi_critical_search));
-	config_search(self, &aa,
 	    CFARGS(.search = axi_search));
 }
 
-/* ARGSUSED */
 static int
-axi_critical_search(device_t parent, struct cfdata *cf,
-    const int *ldesc __unused, void *aux)
+axi_find(device_t parent, cfdata_t cf, const int *ldesc, void *aux)
 {
-	struct axi_attach_args *aa;
+	struct axi_attach_args * const aa = aux;
+	struct axi_attach_args aa0;
 
-	aa = aux;
-
-	if ((strcmp(cf->cf_name, "clk") != 0) &&
-	    (strcmp(cf->cf_name, "imxuart") != 0) &&
-	    (strcmp(cf->cf_name, "imxccm") != 0) &&
-	    (strcmp(cf->cf_name, "imxgpio") != 0))
+	if (strcmp(aa->aa_name, "axi"))
+		return 0;
+	if (aa->aa_addr != AXICF_ADDR_DEFAULT
+	    && aa->aa_addr != cf->cf_loc[AXICF_ADDR])
+		return 0;
+	if (aa->aa_size != AXICF_SIZE_DEFAULT
+	    && aa->aa_size != cf->cf_loc[AXICF_SIZE])
+		return 0;
+	if (aa->aa_intr != AXICF_INTR_DEFAULT
+	    && aa->aa_intr != cf->cf_loc[AXICF_INTR])
+		return 0;
+	if (aa->aa_intrbase != AXICF_IRQBASE_DEFAULT
+	    && aa->aa_intrbase != cf->cf_loc[AXICF_IRQBASE])
 		return 0;
 
-	aa->aa_name = cf->cf_name;
-	aa->aa_addr = cf->cf_loc[AXICF_ADDR];
-	aa->aa_size = cf->cf_loc[AXICF_SIZE];
-	aa->aa_intr = cf->cf_loc[AXICF_INTR];
-	aa->aa_intrbase = cf->cf_loc[AXICF_IRQBASE];
+	aa0 = *aa;
+	aa0.aa_addr = cf->cf_loc[AXICF_ADDR];
+	aa0.aa_size = cf->cf_loc[AXICF_SIZE];
+	aa0.aa_intr = cf->cf_loc[AXICF_INTR];
+	aa0.aa_intrbase = cf->cf_loc[AXICF_IRQBASE];
 
-	if (config_probe(parent, cf, aux))
-		config_attach(parent, cf, aux, axi_print, CFARGS_NONE);
-
-	return 0;
+	return config_match(parent, cf, &aa0);
 }
 
+static const struct {
+	const char *name;
+	bus_addr_t addr;
+	bool required;
+} critical_devs[] = {
+	{ .name = "clk", .addr = 0x96b00000, .required = true }
+};
+
+static void
+axi_attach_critical(struct axi_softc *sc)
+{
+	struct axi_attach_args aa;
+	cfdata_t cf;
+	size_t i;
+
+	for (i = 0; i < __arraycount(critical_devs); i++) {
+		aa.aa_name = "axi";
+		aa.aa_iot = sc->sc_iot;
+		aa.aa_dmat = sc->sc_dmat;
+
+		aa.aa_addr = critical_devs[i].addr;
+		aa.aa_size = AXICF_SIZE_DEFAULT;
+		aa.aa_intr = AXICF_INTR_DEFAULT;
+		aa.aa_intrbase = AXICF_IRQBASE_DEFAULT;
+
+		cf = config_search(sc->sc_dev, &aa,
+		    CFARGS(.submatch = axi_find));
+		if (cf == NULL && critical_devs[i].required)
+			panic("axi_attach_critical: failed to find %s!",
+			    critical_devs[i].name);
+
+		aa.aa_addr = cf->cf_loc[AXICF_ADDR];
+		aa.aa_size = cf->cf_loc[AXICF_SIZE];
+		aa.aa_intr = cf->cf_loc[AXICF_INTR];
+		aa.aa_intrbase = cf->cf_loc[AXICF_IRQBASE];
+		config_attach(sc->sc_dev, cf, &aa, axi_print, CFARGS_NONE);
+	}
+}
 
 /* ARGSUSED */
 static int
